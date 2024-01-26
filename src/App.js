e9ea2a02f0d2d3aef59c04250d32dfc4e82d65e1
Selene Roa <152019449+punkername007@users.noreply.github.com>
2024-01-26 18:34:01 +0100

    Restored files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import "./App.css";
-import Dictionary from "./Dictionary";
-import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <main>
-          <Dictionary />
-        </main>
-      </div>
-      <footer>
-        <Footer />
-      </footer>
+      <header className="App-header">testing</header>
     </div>
   );
 }
