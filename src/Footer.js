f825e02d6a827772f71261f03d911da8c6ec2c5e
Selene Roa <152019449+punkername007@users.noreply.github.com>
2024-01-26 16:39:28 +0100

    Fixed footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,8 +4,18 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="Footer">
-      This project was coded by <a>Selene Roa</a>. It is free sourced on{" "}
-      <a>Github</a> and hosted on <a>Netlify</a>
+      This project was coded by{" "}
+      <a href="#" rel="noreferrer">
+        Selene Roa
+      </a>
+      . It is free sourced on{" "}
+      <a href="#" rel="noreferrer">
+        Github
+      </a>{" "}
+      and hosted on{" "}
+      <a href="#" rel="noreferrer">
+        Netlify
+      </a>
     </div>
   );
 }
